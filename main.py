f6297901ed58352a3fd3f7933fd23c3c4ec266f6
Sergey Kikevich <sergey@rostsber.ru>
2025-08-26 12:15:23 +0300

    chore: add more functions to main.py

--- main.py
+++ main.py
@@ -7,3 +7,9 @@ client = cbr.make_cbr_client()
 #print(client)
 
 print(cbr.get_ruonia_index(client))
+
+print(cbr.currency.get_currencies_list())
+
+print(cbr.currency.get_currency_code("USDRUB"))
+
+print(cbr.currency.get_time_series(symbol="USDRUB", first_date="2019-01-01", last_date="2020-12-31", period="M"))
