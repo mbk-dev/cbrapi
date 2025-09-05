14da6463a20321a745aedc2ac2cc2b0d11a29409
AAARRRT <artiomsalazov@gmail.com>
2025-09-05 04:22:51 +0300

    chore: add more functions

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import cbr
 
-print(cbr.currency.get_currencies_list())
+#print(cbr.currency.get_currencies_list())
 
-client = cbr.make_cbr_client()
+#print(cbr.get_ruonia_index())
 
-#print(client)
+#print(cbr.currency.get_currencies_list())
 
-print(cbr.get_ruonia_index(client))
+#print(cbr.currency.get_currency_code("USDRUB"))
 
-print(cbr.currency.get_currencies_list())
+#print(cbr.currency.get_time_series(symbol="USDRUB", first_date="2019-01-01", last_date="2020-12-31", period="M"))
 
-print(cbr.currency.get_currency_code("USDRUB"))
+#print(cbr.get_key_rate("2017-09-13", "2023-09-13"))
 
-print(cbr.currency.get_time_series(symbol="USDRUB", first_date="2019-01-01", last_date="2020-12-31", period="M"))
+#print(cbr.get_metals_prices())
+
+#print(cbr.get_mrrf("2015-01", "2022-01"))
+
+#print(cbr.get_ibor())
+
+#print(cbr.get_roisfix())
+
+print(cbr.get_ruoniasv())
+
+
