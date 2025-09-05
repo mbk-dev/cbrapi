98b18851b708e68ce665a3952736007fa3698d64
AAARRRT <artiomsalazov@gmail.com>
2025-09-05 04:16:01 +0300

    feat: add new methods for data preprocessing

--- cbr/helpers.py
+++ cbr/helpers.py
@@ -1,5 +1,5 @@
 from typing import Union
-
+from datetime import datetime, date
 import pandas as pd
 
 
@@ -22,3 +22,88 @@ def calculate_inverse_rate(close_ts):
     Inverse close values for currency rate data.
     """
     return 1. / close_ts
+
+def set_datetime_index(data):
+    if not isinstance(data.index, pd.DatetimeIndex):
+        for col in data.columns:
+            if any(keyword in str(col) for keyword in ['CDate', 'DateMet', 'D0', 'DT']):
+                data[col].str.split('T').str[0]
+                data.index = pd.DatetimeIndex(pd.to_datetime(data[col], utc=True)).tz_convert(None)
+                data.index.name = 'DATE'
+                data.drop(col, axis=1, inplace=True)
+                break
+    return data
+    
+def remove_unnecessary_columns(data):
+    data.drop(columns=[col for col in ['id', 'rowOrder'] if col in data.columns], inplace=True)
+    return data 
+    
+def unstack_groups(data, symbol):
+    if symbol == 'DrgMet':       
+        data = data.groupby([data.index, 'CodMet'])['price'].first().unstack()
+        data.columns.name = None
+        
+    if symbol == 'MKR':     
+        data = data.groupby([data.index, 'p1'])[['d1', 'd7', 'd30', 'd90', 'd180', 'd360']].first().unstack(level='p1')
+        data.columns = data.columns.rename(None, level=1)
+        data.columns.name = None
+                
+    return data 
+    
+def column_rename(data, level_0, level_1):
+    if isinstance(data.columns, pd.MultiIndex):
+        if level_0:
+            new_level_0 = data.columns.levels[0].map(
+                lambda x: level_0.get(str(x), x)
+            )
+            data.columns = data.columns.set_levels(new_level_0, level=0)
+            
+        if level_1:
+            new_level_1 = data.columns.levels[1].map(
+                lambda x: level_1.get(str(x), x)
+            )
+            data.columns = data.columns.set_levels(new_level_1, level=1)
+                
+    else:
+        data = data.rename(columns=level_1)
+        available_columns = [col for col in level_1.values() if col in data.columns]
+        data = data[available_columns] if available_columns else data
+    return data
+    
+def normalize_data(data, period, level_0=None,level_1=None, symbol=None):
+    
+    if isinstance(data, pd.Series):
+        data = data.to_frame()    
+
+    set_datetime_index(data)
+    
+    remove_unnecessary_columns(data)  
+      
+    data = unstack_groups(data, symbol)
+
+    data = column_rename(data, level_0, level_1)
+
+    data = pad_missing_periods(data)
+    
+    if period.upper() == 'M':
+        data = data.resample('M').last()
+    
+    if len(data.columns) == 1:
+        data = data.squeeze()  
+    
+    return data
+
+
+def guess_date(input_date, default_value):
+    """
+    Create data in datetime format.
+    CBR accepts "%Y-%m-%d" format only.
+    """
+    if input_date:
+        try:
+            date = datetime.strptime(input_date, "%Y-%m-%d")
+        except ValueError:
+            date = datetime.strptime(input_date, "%Y-%m")
+    else:
+        date = datetime.strptime(default_value, "%Y-%m-%d")
+    return date    
