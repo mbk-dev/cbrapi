ba0be5c679e8d5d3d56eafb4bb1ddf15d43a314c
AAARRRT <artiomsalazov@gmail.com>
2025-09-08 02:50:58 +0300

    fix: delete unncessary function, add docstrings to new methods and improve docstrings to old methods

--- cbr/ruonia.py
+++ cbr/ruonia.py
@@ -6,6 +6,7 @@ import pandas as pd
 from cbr.cbr_settings import make_cbr_client
 from cbr.helpers import normalize_data, guess_date
 
+
 today = date.today()
 
 
@@ -13,84 +14,67 @@ def get_ruonia_ts(symbol: str,
                   first_date: Optional[str] = None,
                   last_date: Optional[str] = None,
                   period: str = 'D') -> pd.Series:
+    """
+    Get RUONIA (Ruble Overnight Index Swap Fixing) time series data for the specified symbol from the Central Bank of Russia.
+    
+    Supported symbols:
+    - RUONIA.INDX - RUONIA index
+    - RUONIA_AVG_1M.RATE - 1-month average rate
+    - RUONIA_AVG_3M.RATE - 3-month average rate  
+    - RUONIA_AVG_6M.RATE - 6-month average rate
+    - Other symbols will return overnight RUONIA rates
+    
+    Args:
+        symbol: Financial instrument symbol
+        first_date: Start date in format 'YYYY-MM-DD' (optional)
+        last_date: End date in format 'YYYY-MM-DD' (optional)
+        period: Data periodicity ('D' for daily, etc.)
+    
+    Returns:
+        pd.Series: Time series data for the requested symbol
+    """
     cbr_client = make_cbr_client()
     if symbol in ['RUONIA.INDX',  'RUONIA_AVG_1M.RATE', 'RUONIA_AVG_3M.RATE',  'RUONIA_AVG_6M.RATE']:
         ticker = symbol.split('.')[0] if symbol.split('.')[1] == 'RATE' else 'RUONIA_INDEX'
-        df = get_ruonia_index(cbr_client, first_date, last_date).loc[:, ticker]
+        df = get_ruonia_index(first_date, last_date).loc[:, ticker]
         if symbol != 'RUONIA.INDX':
             df /= 100
         return normalize_data(df, period, symbol)
     else:
-        return get_ruonia_overnight(cbr_client, first_date, last_date, period)
+        return get_ruonia_overnight(first_date, last_date, period)
 
 
 def get_ruonia_index(first_date: Optional[str] = None,
-                     last_date: Optional[str] = None) -> pd.DataFrame:
+                     last_date: Optional[str] = None,
+                     period: str = 'D') -> pd.DataFrame:
     """
-    Get RUONIA index and averages time series.
+    Get RUONIA (Ruble Overnight Index Swap Fixing) index and averages time series from the Central Bank of Russia.
 
-    Averages: 'RUONIA_AVG_1M', 'RUONIA_AVG_3M', 'RUONIA_AVG_6M'
+    Returns a DataFrame with columns:
+    - RUONIA_INDEX: RUONIA index value
+    - RUONIA_AVG_1M: 1-month average rate
+    - RUONIA_AVG_3M: 3-month average rate  
+    - RUONIA_AVG_6M: 6-month average rate
 
-    RUONIA (Ruble Overnight Index Average) is the weighted average interest rate on interbank loans and deposits.
-    It serves as an indicator of the cost of unsecured overnight borrowing.
+    RUONIA (Ruble Overnight Index Average) is the weighted average interest rate 
+    on interbank loans and deposits. It serves as an indicator of the cost of 
+    unsecured overnight borrowing.
+    
+    Args:
+        first_date: Start date in format 'YYYY-MM-DD' (optional)
+        last_date: End date in format 'YYYY-MM-DD' (optional)
+        period: Data periodicity ('D' for daily, etc.)
+    
+    Returns:
+        pd.DataFrame: Time series data with RUONIA index and averages
     """
     cbr_client = make_cbr_client()
     data1 = guess_date(first_date, default_value='2010-01-01')
-    data2 = guess_date(last_date, default_value=str(date.today()))
-    ruonia_xml = cbr_client.service.RuoniaSV(data1, data2)
-    try:
-        df = pd.read_xml(ruonia_xml, xpath="//ra")
-    except ValueError:
-        return pd.Series()
-    df.drop(columns=['id', 'rowOrder'], inplace=True)
-    df = df.astype({'DT': 'period[D]'}, copy=False)
-    df.columns = [s.upper() for s in df.columns]
-    float_columns = ['RUONIA_INDEX', 'RUONIA_AVG_1M', 'RUONIA_AVG_3M', 'RUONIA_AVG_6M']
-    df[float_columns] = df[float_columns].astype('float', copy=False)
-    df.set_index('DT', inplace=True, verify_integrity=True)
-    df.sort_index(ascending=True, inplace=True)
-    df.index.rename('date', inplace=True)
-    return df
-
-
-def get_ruonia_overnight(first_date: Optional[str] = None,
-                         last_date: Optional[str] = None,
-                         period: str = 'D') -> pd.Series:
-    """
-    Get RUONIA overnight value time series.
-
-    RUONIA (Ruble Overnight Index Average) is the weighted average interest rate on interbank loans and deposits.
-    It serves as an indicator of the cost of unsecured overnight borrowing.
-    """
-    cbr_client = make_cbr_client()
-    data1 = guess_date(first_date, default_value='2010-01-01')
-    data2 = guess_date(last_date, default_value=str(date.today()))
-    ruonia_xml = cbr_client.service.Ruonia(data1, data2)
-    try:
-        df = pd.read_xml(ruonia_xml, xpath="//ro")
-    except ValueError:
-        return pd.Series()
-    df.drop(columns=['id', 'rowOrder', 'vol', 'DateUpdate'], inplace=True)
-    df = df.astype({'D0': 'period[D]'}, copy=False)
-    float_columns = ['ruo']
-    df[float_columns] = df[float_columns].astype('float', copy=False)
-    df.set_index('D0', inplace=True, verify_integrity=True)
-    df.sort_index(ascending=True, inplace=True)
-    df /= 100
-    return normalize_data(df, period, 'RUONIA.RATE')
-    
-    
-def get_ruoniasv(first_date: Optional[str] = None,
-                last_date: Optional[str] = None,
-                 period: str = 'D') -> pd.Series:    
-                  
-    cbr_client = make_cbr_client()
-    data1 = guess_date(first_date, default_value='2010-04-15')   
     data2 = guess_date(last_date, default_value=str(today))
-    ruoniasv_xml = cbr_client.service.RuoniaSV(data1, data2)
+    ruonia_index_xml = cbr_client.service.RuoniaSV(data1, data2)
     
     try:
-        df = pd.read_xml(ruoniasv_xml, xpath=".//ra")
+        df = pd.read_xml(ruonia_index_xml, xpath=".//ra")
     except ValueError:
         return pd.Series()
         
@@ -102,13 +86,70 @@ def get_ruoniasv(first_date: Optional[str] = None,
         }  
                                    
     df = normalize_data(data=df, period=period, symbol='ra', level_1=level_1_column_mapping)     
-    return df    
+    return df 
+
+
+def get_ruonia_overnight(first_date: Optional[str] = None,
+                         last_date: Optional[str] = None,
+                         period: str = 'D') -> pd.Series:
+    """
+    Get RUONIA (Ruble Overnight Index Swap Fixing) overnight value time series from the Central Bank of Russia.
+
+    RUONIA (Ruble Overnight Index Average) is the weighted average interest rate 
+    on interbank loans and deposits. It serves as an indicator of the cost of 
+    unsecured overnight borrowing.
+    
+    Args:
+        first_date: Start date in format 'YYYY-MM-DD' (optional)
+        last_date: End date in format 'YYYY-MM-DD' (optional)
+        period: Data periodicity ('D' for daily, etc.)
+    
+    Returns:
+        pd.Series: Time series of RUONIA overnight rates (as decimals, e.g., 0.05 for 5%)
+    """
+    cbr_client = make_cbr_client()
+    data1 = guess_date(first_date, default_value='2010-01-01')
+    data2 = guess_date(last_date, default_value=str(date.today()))
+    ruonia_overnight_xml = cbr_client.service.Ruonia(data1, data2)
+    
+    try:
+        df = pd.read_xml(ruonia_overnight_xml, xpath="//ro")
+    except ValueError:
+        return pd.Series()
+
+    level_1_column_mapping = {
+        'ruo': 'RUONIA_OVERNIGHT',
+        } 
+        
+    df = normalize_data(data=df, period=period, symbol='ro', level_1=level_1_column_mapping)  
+    df /= 100
+    return df
     
     
 def get_roisfix(first_date: Optional[str] = None,
                 last_date: Optional[str] = None,
-                 period: str = 'D') -> pd.Series:       
-                  
+                 period: str = 'D') -> pd.DataFrame:       
+    """
+    Get ROISfix (Ruble Overnight Index Swap Fixing) time series data from the Central Bank of Russia.
+    
+    Returns a DataFrame with various term rates:
+    - RATE_1_WEEK: 1-week rate
+    - RATE_2_WEEK: 2-week rate  
+    - RATE_1_MONTH: 1-month rate
+    - RATE_2_MONTH: 2-month rate
+    - RATE_3_MONTH: 3-month rate
+    - RATE_6_MONTH: 6-month rate
+    
+    ROISfix represents the fixed rate in ruble overnight index swaps.
+    
+    Args:
+        first_date: Start date in format 'YYYY-MM-DD' (optional)
+        last_date: End date in format 'YYYY-MM-DD' (optional)
+        period: Data periodicity ('D' for daily, etc.)
+    
+    Returns:
+        pd.DataFrame: Time series data with ROISfix rates for different terms
+    """                     
     cbr_client = make_cbr_client()
     data1 = guess_date(first_date, default_value='2011-04-15')   
     data2 = guess_date(last_date, default_value=str(today))
@@ -118,13 +159,15 @@ def get_roisfix(first_date: Optional[str] = None,
         df = pd.read_xml(roisfix_xml, xpath=".//rf")
     except ValueError:
         return pd.Series()
+    
+    print(df)
         
     level_1_column_mapping = {
         'R1W': 'RATE_1_WEEK',
         'R2W': 'RATE_2_WEEK',
         'R1M': 'RATE_1_MONTH',
-        'R1M': 'RATE_2_MONTH', 
-        'R2M': 'RATE_3_MONTH',   
+        'R2M': 'RATE_2_MONTH', 
+        'R3M': 'RATE_3_MONTH',   
         'R6M': 'RATE_6_MONTH'
         }  
                                    
